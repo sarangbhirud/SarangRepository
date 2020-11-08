e96656bc84b44650866f5b3139b625542178d533
sarang <sbhirud@gmail.com>
2020-11-08 21:28:24 +0530

    first commit

---
+++ src/test/java/Mavenjava2/RESTAPITest.java
@@ -0,0 +1,25 @@
+package Mavenjava2;
+
+
+import org.testng.annotations.Test;
+
+	
+
+	//rahulonlinetutor@gmail.com
+	public class RESTAPITest {
+
+		@Test
+		public void postJira()
+		{
+			System.out.println("postJira");
+		}
+		
+		@Test
+		public void deleteTwitter()
+		{
+			System.out.println("deleteTwitter");	
+		}
+	}
+
+
+
